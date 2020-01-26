dc98f6037b11c04e8cee18ce69c0425c8f8f6abb
nenniuwufang <2212559103@qq.com>
2020-01-27 00:02:59 +0800

    update
    update dictionary's operation, some of Day3's code

---
+++ Day2/5DictTset.py
@@ -0,0 +1,17 @@
+# 字典的使用规范
+# 字典的结构：
+# 字典的结构一般为{key：value}的形式
+# 调用key会返回value的值 value的值可以限定为默认值
+
+dict = {"name":"小明","age":20,"sex":"male"}
+# 此为字典的赋值方式
+i = dict.get("name")
+# 此为获取字典key后值的形式之一
+# 该方法不对字典本身做出任何更改
+print(i)
+print(dict)
+j = dict.pop("name")
+# 此为获取键名称为“name”的值的形式之二
+# 该方法会对于字典本身造成更改
+print(j)
+print(dict)
